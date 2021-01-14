e95b1498559e465af0fefd4afc49d78ab4d2c7fd
Z <mohistzh@gmail.com>
2021-01-14 20:36:41 +0800

    add guess

---
+++ src/guess.rs
@@ -0,0 +1,26 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    loop {
+        print!("Please input your guess:");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read input");
+        let input: u32 = match input.trim().parse::<u32>() {
+            Ok(number) => number,
+            Err(_) => continue,
+        };
+        println!("Your guessed: {}", input);
+        match input.cmp(&secret_number) {
+            Ordering::Less => println!("Your guess is too small"),
+            Ordering::Greater => println!("Your guess is too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
