4a385a0ee2b5a278d72f9c207aa21ed124723ac9
Z <mohistzh@gmail.com>
2021-01-14 21:30:53 +0800

    added ut

--- src/temperature.rs
+++ src/temperature.rs
@@ -5,14 +5,29 @@ fn ft_to_ct(ft: u32) -> u32 {
         println!("Fahrenheit temperature can not be negative");
         os.Exit(-1);
     }
-    (ft - 32) / 1.8
+    return (ft - 32) / 1.8;
 }
 fn ct_to_ft(ct: u32) -> u32 {
     if ct < 0 {
         println!("Celsius temperature can not be negative");
         os.Exit(-1);
     }
-    32 + ct * 1.8
+    return 32 + ct * 1.8;
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
 
+    #[test]
+    fn ft_to_ct_test() {
+        let ct = ft_to_ct(22);
+        println!("CT is {}", ct);
+    }
+
+    #[test]
+    fn ct_to_ft_test() {
+        ct_to_ft(22);
+    }
+
+}
