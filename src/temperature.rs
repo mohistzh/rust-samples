6d92d7d028a8785c7068a47322da443f44a695fb
Z <mohistzh@gmail.com>
2021-01-14 21:55:08 +0800

    update ut

--- src/temperature.rs
+++ src/temperature.rs
@@ -1,13 +1,13 @@
 //Fahrenheit temperature and Celsius temperature
 
-fn ft_to_ct(ft: u32) -> u32 {
+pub fn ft_to_ct(ft: u32) -> u32 {
     if ft < 0 {
         println!("Fahrenheit temperature can not be negative");
         os.Exit(-1);
     }
     return (ft - 32) / 1.8;
 }
-fn ct_to_ft(ct: u32) -> u32 {
+pub fn ct_to_ft(ct: u32) -> u32 {
     if ct < 0 {
         println!("Celsius temperature can not be negative");
         os.Exit(-1);
